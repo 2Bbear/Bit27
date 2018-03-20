7cc74b80e94806f74985fe5f1e42053b94225b82
2Bbear <wkdwjdgh1234@naver.com>
2018-03-20 23:23:00 +0900

    소켓분석
    소켓분석

---
+++ API/통신소켓분석중/Project4/Project4/wbnet.h
@@ -0,0 +1,6 @@
+#pragma once
+//wbnet.h
+// 사용자 정의 데이터 수신 함수
+int recvn(SOCKET s, char *buf, int len, int flags);
+
+DWORD WINAPI foo(void* p);
---
+++ API/통신소켓분석중/Project5/Project5/packet.h
@@ -0,0 +1,6 @@
+#pragma once
+typedef struct tagDATA
+{
+	int flag;
+	char msg[1024];
+}DATA;
