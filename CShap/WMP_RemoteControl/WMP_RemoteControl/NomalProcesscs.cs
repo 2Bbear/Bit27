aabbe951496380135967b53373e9c77c2173ddb4
2Bbear <wkdwjdgh1234@naver.com>
2018-05-16 08:58:08 +0900

    wkdwjdgh1234
    wkdwjdgh1234

---
+++ CShap/WMP_RemoteControl/WMP_RemoteControl/NomalProcesscs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WMP_RemoteControl
+{
+    class NomalProcesscs
+    {
+        //dll
+        #region 일반 process 접근용
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName); // 현재 켜져있는 프로세스 검색
+        [DllImport("user32.dll")]
+        public static extern int FindWindowEx(int hWnd1, int hWnd2, string lpsz1, string lpsz2); // 해당 프로세스 내부에 있는 자식 프로세스 검색
+        [DllImport("user32")]
+        public static extern int SendMessage(int hwnd, int wMsg, int wParam, int lParam); // 메세지 보내는 함수
+        #endregion
+
+
+        //title bar name으로 현재 윈도우를 찾는 함수
+        private void StartProgram(string programpath)
+        {
+            int nhwnd = FindWindow(null, programpath);    // 윈도우 창 제목
+            MessageBox.Show(nhwnd.ToString());
+
+            int hw1 = FindWindowEx(nhwnd, 0, "Edit", "");
+            MessageBox.Show(hw1.ToString());
+            SendMessage(hw1, 0X000C, 0, 0);
+        }
+    }
+    
+}
+
---
+++ CShap/WMP_RemoteControl/WMP_RemoteControl/NotePadProcess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMP_RemoteControl
+{
+    class NotePadProcess
+    {
+        #region Imports
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("User32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        //this is a constant indicating the window that we want to send a text message
+        const int WM_SETTEXT = 0X000C;
+        #endregion
+
+
+        public Process StartProgram(string insulttxt)
+        {
+            Process notepad = Process.Start(@"notepad.exe");
+            System.Threading.Thread.Sleep(500);
+            IntPtr notepadTextbox = FindWindowEx(notepad.MainWindowHandle, IntPtr.Zero, "Edit", null);
+            //SendMessage(notepadTextbox, Convert.ToInt32("0X000C",16), 0, insulttxt);
+            SendMessage(notepadTextbox, 0X000C, 0, insulttxt);
+            return notepad;
+            //SendMessage(notepadTextbox, WM_SETTEXT, 0, insulttxt);
+            //SendMessage(notepadTextbox, 0101, Convert.ToInt32("00000044",16), "C0200001");
+        }
+    }
+}
